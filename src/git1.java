01a664452305e46c8e04da9f5ba079805c9b5067
username <ee@qq.com>
2022-04-09 12:20:37 +0800

    frist commint1

--- src/git1.java
+++ src/git1.java
@@ -8,5 +8,6 @@ public class git1 {
         System.out.println("hotfix test");
         System.out.println("he7");
         System.out.println("66666666");
+        System.out.println("777777777");
     }
 }
